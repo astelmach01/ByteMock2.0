9fff5648be8098aff416f6114fcc6a1521933678
Andrew Stelmach <ukiandre01@outlook.com>
2020-06-10 15:31:35 -0400

    Added header to copy website

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import logo from "../BYTEMOCK_LOGO.png"
+class Header extends React.Component{
+    constructor(props) {
+        super (props)
+        this.state = {
+            isLoading: false
+        }
+
+    }
+
+    render(){
+
+        return (
+            <div className="Header">
+
+            </div>
+
+        )}
+
+
+}
+
+export default Header
